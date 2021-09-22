e811935a118318755de1cd455c0b1a8a72fd108c
sun.yaopeng <906262260@qq.com>
2021-09-22 17:17:04 +0800

    test husky

--- apps/sarair/src/main.tsx
+++ apps/sarair/src/main.tsx
@@ -3,14 +3,13 @@ import * as ReactDOM from 'react-dom'
 
 import App from './app/app'
 
+
+
+
+
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-
-
-
-
-
   document.getElementById('root')
 )
