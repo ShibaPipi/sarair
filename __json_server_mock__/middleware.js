2600cc31ad26e31545e841d8e16aca250d32d6e7
sun.yaopeng <906262260@qq.com>
2021-10-01 23:35:31 +0800

    feat: change response data

--- __json_server_mock__/middleware.js
+++ __json_server_mock__/middleware.js
@@ -2,9 +2,8 @@ module.exports = (request, response, next) => {
   if (request.method === 'POST' && request.path === '/login') {
     if (request.body.username === 'pdrol' && request.body.password === '111111') {
       return response.status(200).json({
-        user: {
-          token: '123'
-        }
+        name: request.body.username,
+        token: '123'
       })
     } else {
       return response.status(400).json({ message: 'invalid username or password' })
