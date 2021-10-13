245675403551e0e967c1b0f5c5016496ede821d1
sun.yaopeng <906262260@qq.com>
2021-10-13 10:24:22 +0800

    feat: add ui components

--- libs/shared/ui/src/lib/Table/index.ts
+++ libs/shared/ui/src/lib/Table/index.ts
@@ -1,5 +1,9 @@
-import Table, { ColumnProps as ColumnPropsType } from 'antd/es/table'
+import Table, {
+  ColumnProps as ColumnPropsType,
+  TableProps as TablePropsType
+} from 'antd/es/table'
 import 'antd/es/table/style/css'
 
 export type ColumnProps<T> = ColumnPropsType<T>
+export type TableProps<T> = TablePropsType<T>
 export { Table }
