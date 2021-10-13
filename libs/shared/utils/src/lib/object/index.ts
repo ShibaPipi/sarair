b2f2c2fa50ab908261301c6ef4a6ba4600ddc8a3
sun.yaopeng <906262260@qq.com>
2021-10-13 10:25:06 +0800

    feat: add utils function

--- libs/shared/utils/src/lib/object/index.ts
+++ libs/shared/utils/src/lib/object/index.ts
@@ -1,19 +1,17 @@
-interface CommonObject {
-  [key: string]: any
+export interface CommonObject {
+  [key: string]: unknown
 }
 
-export const isFalsy = (value: unknown) => {
-  if (value === 0) {
-    return false
-  }
-  return !value
-}
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value)
+
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === ''
 
 export function cleanObjectNilValue(object: CommonObject) {
   const resultObject = { ...object }
 
   Object.keys(object).forEach((key) => {
-    if (isFalsy(object[key])) {
+    if (isVoid(object[key])) {
       delete resultObject[key]
     }
   })
