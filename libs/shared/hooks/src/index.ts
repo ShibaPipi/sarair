e94316ea5cde2e0f9fe8e74660e27a53d618c249
sun.yaopeng <906262260@qq.com>
2021-12-21 17:47:19 +0800

    feat: add shared hooks

--- libs/shared/hooks/src/index.ts
+++ libs/shared/hooks/src/index.ts
@@ -1,3 +1,6 @@
 export * from './lib/useAsync'
+export * from './lib/useAntdTable'
 export * from './lib/useDebounce'
+export * from './lib/useMemoizedFn'
 export * from './lib/useMount'
+export * from './lib/useRequest'
---
+++ libs/shared/hooks/src/lib/useAntdTable/index.ts
@@ -0,0 +1,1 @@
+export { useAntdTable } from 'ahooks'
