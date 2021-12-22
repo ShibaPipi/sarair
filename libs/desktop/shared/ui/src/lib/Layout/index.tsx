015a50a2f013718089750df3a96b2bc920fe921f
sun.yaopeng <906262260@qq.com>
2021-12-22 16:29:24 +0800

    feat: add desktop shared ui

---
+++ libs/desktop/shared/ui/src/lib/Layout/index.tsx
@@ -0,0 +1,8 @@
+import Layout from 'antd/es/layout'
+import 'antd/es/layout/style/css'
+import Col from 'antd/es/col'
+import 'antd/es/col/style/css'
+import Row from 'antd/es/row'
+import 'antd/es/row/style/css'
+
+export { Layout, Col, Row }
