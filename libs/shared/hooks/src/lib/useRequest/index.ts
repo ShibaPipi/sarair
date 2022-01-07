34e8d3530fa728e58c295955973cc4a83cc0cec0
sun.yaopeng <906262260@qq.com>
2022-01-07 19:41:20 +0800

    feat: add shared hook useUrlQueryState

--- libs/shared/hooks/src/lib/useRequest/index.ts
+++ libs/shared/hooks/src/lib/useRequest/index.ts
@@ -1,1 +1,9 @@
-export { useRequest } from 'ahooks'
+import { useRequest as useRequestBase } from 'ahooks'
+import { Options, Service } from 'ahooks/lib/useRequest/src/types'
+
+export const useRequest = <TData, TParams extends unknown[]>(
+    service: Service<TData, TParams>,
+    options?: Options<TData, TParams>
+) => {
+    return useRequestBase(service, options)
+}
