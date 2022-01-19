78abf7d36b8cb68894d24535ccd9cdeec0d595bc
sun.yaopeng <906262260@qq.com>
2022-01-19 14:35:09 +0800

    feat: add new health data

---
+++ libs/desktop/shared/ui/src/lib/Modal/index.tsx
@@ -0,0 +1,8 @@
+import BaseModal from 'antd/es/modal'
+import 'antd/es/modal/style/css'
+import React from "react";
+
+// export { Modal }
+const Modal:React.FC = () => <BaseModal></BaseModal>
+
+export { Modal }
