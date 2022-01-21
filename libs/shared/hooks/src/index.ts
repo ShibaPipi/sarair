25e5e7c44f85aab0c82ea6bc9c5429fee2ef4437
sun.yaopeng <906262260@qq.com>
2022-01-21 15:52:33 +0800

    feat: remove hook useUrlQueryState

--- libs/shared/hooks/src/index.ts
+++ libs/shared/hooks/src/index.ts
@@ -4,7 +4,6 @@ export * from './lib/useAntdTable'
 export * from './lib/useDebounce'
 export * from './lib/useMemoizedFn'
 export * from './lib/useMount'
-export * from './lib/useUrlQueryState'
 export * from './lib/useUrlState'
 
 export * from './lib/useListRequest'
