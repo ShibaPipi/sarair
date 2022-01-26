e9406ef454ebe1ff23ff38130cba5aea0bb3b03c
sun.yaopeng <906262260@qq.com>
2022-01-26 11:58:39 +0800

    feat(desktop-shared-ui): add new components

--- libs/desktop/shared/ui/src/index.ts
+++ libs/desktop/shared/ui/src/index.ts
@@ -4,4 +4,5 @@ export * from './lib/antd'
 // custom component exports
 export * from './lib/FullPageErrorCallback'
 export * from './lib/IdSelector'
+export * from './lib/Pin'
 export * from './lib/SarairRow'
---
+++ libs/desktop/shared/ui/src/lib/Pin/index.tsx
@@ -0,0 +1,23 @@
+import React, { ComponentProps } from 'react'
+
+import { Rate } from '../antd'
+
+interface PinProps extends ComponentProps<typeof Rate> {
+    checked: boolean
+    onCheckedChange?: (checked: boolean) => void
+}
+
+export const Pin: React.FC<PinProps> = ({
+    checked,
+    onCheckedChange,
+    ...props
+}) => {
+    return (
+        <Rate
+            count={1}
+            value={checked ? 1 : 0}
+            onChange={num => onCheckedChange?.(!!num)}
+            {...props}
+        />
+    )
+}
