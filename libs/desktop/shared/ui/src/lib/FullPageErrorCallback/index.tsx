76c8dedc2690d288d3e06b50328b78ec6ed6c6e0
sun.yaopeng <906262260@qq.com>
2022-02-07 15:40:59 +0800

    refactor(desktop-shared-ui): change import of TypographyText

--- libs/desktop/shared/ui/src/lib/FullPageErrorCallback/index.tsx
+++ libs/desktop/shared/ui/src/lib/FullPageErrorCallback/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import type { ErrorBoundaryType } from '@sarair/shared/types'
 
 import { FullPage } from '@sarair/shared/ui'
-import { TypographyText } from '../Typography'
+import { TypographyText } from '../antd'
 
 export const FullPageErrorCallback: React.FC<ErrorBoundaryType> = ({
     error
