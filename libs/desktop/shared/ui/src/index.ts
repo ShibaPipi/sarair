d0b92eb658164480e9e0212b0a9f225d18e0db49
sun.yaopeng <906262260@qq.com>
2022-02-09 15:52:35 +0800

    feat(desktop-shared-ui): add custom component

--- libs/desktop/shared/ui/src/index.ts
+++ libs/desktop/shared/ui/src/index.ts
@@ -2,6 +2,7 @@
 export * from './lib/antd'
 
 // custom component exports
+export * from './lib/ButtonNoPadding'
 export * from './lib/FullPageErrorCallback'
 export * from './lib/IdSelector'
 export * from './lib/Pin'
---
+++ libs/desktop/shared/ui/src/lib/ButtonNoPadding/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+import { Button } from '@sarair/desktop/shared/ui'
+
+export const ButtonNoPadding = styled(Button)`
+    padding: 0;
+`
