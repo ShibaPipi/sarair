c3a324a859206764a7065f37aa61e2ddcf779381
sun.yaopeng <906262260@qq.com>
2022-02-15 15:42:54 +0800

    refactor(hooks): refactor with useReducer

--- libs/shared/hooks/src/lib/useAsync/index.ts
+++ libs/shared/hooks/src/lib/useAsync/index.ts
@@ -1,4 +1,7 @@
-import { useCallback, useState } from 'react'
+import { useReducer, useState } from 'react'
+
+import { useMemoizedFn } from '../useMemoizedFn'
+import { useUnmountedRef } from '../useUnmountedRef'
 
 enum StatusEnum {
     IDLE = 'idle',
@@ -19,43 +22,89 @@ const defaultInitialState: State<null> = {
     error: null
 }
 
-export const useAsync = <D>(initialState?: Partial<State<D>>) => {
-    const [state, setState] = useState<State<D>>({
-        ...defaultInitialState,
-        ...initialState
-    })
+const defaultConfig = {
+    throwOnError: false
+}
 
-    const setData = (data: D) =>
-        setState({
+const useSafeDispatch = <T>(dispatch: (...args: T[]) => void) => {
+    const unmountedRef = useUnmountedRef()
+
+    return useMemoizedFn((...args: T[]) =>
+        unmountedRef.current ? void 0 : dispatch(...args)
+    )
+}
+
+export const useAsync = <D>(
+    initialState?: Partial<State<D>>,
+    initialConfig?: typeof defaultConfig
+) => {
+    const config = { ...defaultConfig, ...initialConfig }
+
+    const [state, dispatch] = useReducer<
+        (state: State<D>, action: Partial<State<D>>) => State<D>
+    >(
+        (state: State<D>, action: Partial<State<D>>) => ({
+            ...state,
+            ...action
+        }),
+        {
+            ...defaultInitialState,
+            ...initialState
+        }
+    )
+
+    const safeDispatch = useSafeDispatch(dispatch)
+
+    const [retry, setRetry] = useState<() => void>(() => () => void 0)
+
+    const setData = useMemoizedFn((data: D) =>
+        safeDispatch({
             status: StatusEnum.SUCCESS,
             data,
             error: null
         })
+    )
 
-    const setError = (error: Error) =>
-        setState({
+    const setError = useMemoizedFn((error: Error) =>
+        safeDispatch({
             status: StatusEnum.ERROR,
             data: null,
             error
         })
+    )
 
-    const run = useCallback((promise: Promise<D>) => {
-        if (!promise || !promise.then) {
-            throw new Error('The parameter promise must be a Promise')
+    const run = useMemoizedFn(
+        (promise: Promise<D>, runConfig?: { retry: () => Promise<D> }) => {
+            if (!promise || !promise.then) {
+                throw new Error('The parameter promise must be a Promise')
+            }
+
+            setRetry(() => () => {
+                if (runConfig?.retry) {
+                    run(runConfig?.retry(), runConfig)
+                }
+            })
+
+            safeDispatch({ status: StatusEnum.LOADING })
+
+            return promise
+                .then(data => {
+                    setData(data)
+                    return data
+                })
+                .catch(error => {
+                    setError(error)
+
+                    // catch 会消化异常，如果不主动抛出，外面是接收不到异常的
+                    if (config.throwOnError) return Promise.reject(error)
+
+                    return error
+                })
         }
-        setState({ ...state, status: StatusEnum.LOADING })
-        return promise
-            .then((data) => {
-                setData(data)
-                return data
-            })
-            .catch((error) => {
-                setError(error)
-                return error
-            })
-    }, [])
+    )
 
     return {
+        ...state,
         isIdle: state.status === StatusEnum.IDLE,
         isLoading: state.status === StatusEnum.LOADING,
         isSuccess: state.status === StatusEnum.SUCCESS,
@@ -63,8 +112,9 @@ export const useAsync = <D>(initialState?: Partial<State<D>>) => {
         methods: {
             run,
             setData,
-            setError
-        },
-        ...state
+            setError,
+            // retry 被调用时重新跑一遍run，让state刷新一遍
+            retry
+        }
     }
 }
