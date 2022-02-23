8a83bd25bb6e7a9cd145e4b38bbc9b4ba31a455c
sun.yaopeng <906262260@qq.com>
2022-02-23 10:39:45 +0800

    refactor(jira app): redo context from redux-toolkit

--- apps/jira/src/providers/auth.tsx
+++ apps/jira/src/providers/auth.tsx
@@ -1,14 +1,14 @@
-import React, { ReactNode } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { ReactNode, useState } from 'react'
+// import { useDispatch } from 'react-redux'
 
-// import * as auth from '@sarair/shared/auth'
+import * as auth from '@sarair/shared/auth'
 import { useMount } from '@sarair/shared/hooks'
-// import { AuthContext } from '@sarair/shared/context'
+import { AuthContext } from '@sarair/shared/context'
 import { sarairRequest } from '@sarair/shared/request'
 import { getToken } from '@sarair/shared/utils'
-import { bootstrap } from '../store/auth.slice'
+// import { bootstrap } from '../store/auth.slice'
 
-import type { User } from '@sarair/shared/context'
+import type { AuthForm, User } from '@sarair/shared/context'
 
 export const bootstrapUser = async () => {
     if (!getToken()) return null
@@ -18,37 +18,37 @@ export const bootstrapUser = async () => {
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     // 使用 context
-    // const [user, setUser] = useState<User | null>(null)
-    //
-    // // point free
-    // const login = (form: AuthForm) => auth.login(form).then(setUser)
-    // const register = (form: AuthForm) => auth.register(form).then(setUser)
-    // const logout = () => auth.logout().then(() => setUser(null))
-    //
-    // useMount(() => {
-    //     bootstrapUser().then(setUser)
-    // })
-    //
-    // return (
-    //     <AuthContext.Provider
-    //         children={children}
-    //         value={{
-    //             user,
-    //             methods: {
-    //                 login,
-    //                 register,
-    //                 logout
-    //             }
-    //         }}
-    //     />
-    // )
+    const [user, setUser] = useState<User | null>(null)
 
-    // 使用 redux
-    const dispatch = useDispatch<(...args: unknown[]) => Promise<User>>()
+    // point free
+    const login = (form: AuthForm) => auth.login(form).then(setUser)
+    const register = (form: AuthForm) => auth.register(form).then(setUser)
+    const logout = () => auth.logout().then(() => setUser(null))
 
     useMount(() => {
-        dispatch(bootstrap())
+        bootstrapUser().then(setUser)
     })
 
-    return <div>{children}</div>
+    return (
+        <AuthContext.Provider
+            children={children}
+            value={{
+                user,
+                methods: {
+                    login,
+                    register,
+                    logout
+                }
+            }}
+        />
+    )
+
+    // 使用 redux
+    // const dispatch = useDispatch<(...args: unknown[]) => Promise<User>>()
+    //
+    // useMount(() => {
+    //     dispatch(bootstrap())
+    // })
+    //
+    // return <div>{children}</div>
 }
