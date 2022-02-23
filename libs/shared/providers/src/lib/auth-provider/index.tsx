8b96652a9ce72d5fd3da02c665ca57b5a0f1e224
sun.yaopeng <906262260@qq.com>
2022-02-23 10:18:49 +0800

    feat(shared provider): remove references

--- libs/shared/providers/src/lib/auth-provider/index.tsx
+++ libs/shared/providers/src/lib/auth-provider/index.tsx
@@ -1,36 +1,16 @@
-import React, { ReactNode, useState } from 'react'
+import React, { ReactNode } from 'react'
 
-import * as auth from '@sarair/shared/auth'
-import { useMount } from '@sarair/shared/hooks'
-import { AuthContext } from '@sarair/shared/context'
 import { sarairRequest } from '@sarair/shared/request'
 import { getToken } from '@sarair/shared/utils'
 
-import type { AuthForm, User } from '@sarair/shared/context'
+import type { User } from '@sarair/shared/context'
+
+export const bootstrapUser = async () => {
+    if (!getToken()) return null
+
+    return await sarairRequest.get<User>('me')
+}
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-    const [user, setUser] = useState<User | null>(null)
-
-    // point free
-    const login = (form: AuthForm) => auth.login(form).then(setUser)
-    const register = (form: AuthForm) => auth.register(form).then(setUser)
-    const logout = () => auth.logout().then(() => setUser(null))
-
-    useMount(() => {
-        getToken() && sarairRequest.get<User>('me').then(setUser)
-    })
-
-    return (
-        <AuthContext.Provider
-            children={children}
-            value={{
-                user,
-                methods: {
-                    login,
-                    register,
-                    logout
-                }
-            }}
-        />
-    )
+    return <div>{children}</div>
 }
