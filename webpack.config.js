010f4edc2fe51db3579bc97dbcc2f8ea822a66fc
sun.yaopeng <906262260@qq.com>
2022-06-10 23:58:55 +0800

    fix(2048): fix build failed

--- webpack.config.js
+++ webpack.config.js
@@ -19,44 +19,6 @@ module.exports = config => {
 
     return {
         ...config,
-        ...customConfig,
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    vendor: {
-                        name: 'vendor',
-                        test: /[\\/]node_modules[\\/]/,
-                        chunks: 'all',
-                        priority: 10,
-                        enforce: true,
-                    },
-                    react: {
-                        name: 'react',
-                        test: module => /react|redux/.test(module.context),
-                        chunks: 'initial',
-                        priority: 11,
-                        enforce: true,
-                    },
-                    antd: {
-                        name: 'antd',
-                        test: (module) => {
-                            return /ant/.test(module.context);
-                        },
-                        chunks: 'initial',
-                        priority: 11,
-                        enforce: true,
-                    },
-                    moment: {
-                        name: 'moment',
-                        test: (module) => {
-                            return /moment/.test(module.context);
-                        },
-                        chunks: 'initial',
-                        priority: 13,
-                        enforce: true,
-                    }
-                }
-            }
-        }
+        ...customConfig
     }
 }
